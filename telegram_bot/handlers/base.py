741642b3f794528e2e5b40a72d997c65d789b653
Pncheeck <159966542+Pncheek@users.noreply.github.com>
2025-06-27 21:35:19 +0300

    Update base.py

--- telegram_bot/handlers/base.py
+++ telegram_bot/handlers/base.py
@@ -2,8 +2,13 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 class BaseHandler:
     BACK_BUTTON_CALLBACK = "back_to_start"
+
+    ALLOWED_USER_IDS = {562442831, 354135340, 222459211, 2028809889}  
     
-    def __init__(self, db, scheduler=None):  # Добавляем scheduler как необязательный параметр
+    def is_user_allowed(self, user_id: int) -> bool:
+        return user_id in self.ALLOWED_USER_IDS
+    
+    def __init__(self, db, scheduler=None):  
         self.db = db
         self.scheduler = scheduler
     
